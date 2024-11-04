ab30b3809997e384d8a3f358c1ddc335c54e9a03
rlbeagley <rbeagleyuni@gmail.com>
2024-11-03 20:56:03 -0500

    msgs added when client connects/disconnects

--- edu/seg2105/edu/server/backend/EchoServer.java
+++ edu/seg2105/edu/server/backend/EchoServer.java
@@ -72,6 +72,14 @@ public class EchoServer extends AbstractServer
       ("Server has stopped listening for connections.");
   }
   
+  protected void clientConnected(ConnectionToClient client) {
+	  System.out.println("Client has connected!");
+  }
+  
+  synchronized protected void clientDisconnected(ConnectionToClient client) {
+	  System.out.println("Client has disconnected. ");
+  }
+  
   
   //Class methods ***************************************************
   
